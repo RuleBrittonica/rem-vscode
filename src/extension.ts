b40750a8c668df1ad9fc251e3624a07b43f4cbb0
RuleBrittonica <mbritton460@gmail.com>
2024-08-13 13:49:22 +1000

    Initial framework for extension laid out

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,131 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('remvscode.refactor', async () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "remvscode" is now active!');
+    const selectedText = editor.document.getText(editor.selection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('remvscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from rem-vscode!');
-	});
+    // Display a dropdown menu with the options to refactor
+    const options = [
+      'Variable',
+      'Function (fn)',
+      'Struct',
+      'Trait',
+      'Implementation (impl)',
+    ];
+    const selectedOption = await vscode.window.showQuickPick(options, {
+      placeHolder: 'Select an option to begin refactoring'
+    });
 
-	context.subscriptions.push(disposable);
+    if (!selectedOption) {
+      return; // User canceled the dropdown
+    }
+
+    // Get the prompt and placeholder text for the original name
+    const { promptText: originalPrompt, placeholderText: originalPlaceholder } = getPandP_original(selectedOption);
+
+    // Text field for name of the field to be refactored
+    const originalNameTextField = await vscode.window.showInputBox({
+      prompt: originalPrompt,
+      placeHolder: originalPlaceholder,
+    });
+
+    if (!originalNameTextField) {
+      return; // User canceled the input
+    }
+
+    // Get the prompt and placeholder text for the new name
+    const { promptText: newPrompt, placeholderText: newPlaceholder } = getPandP_new(selectedOption);
+
+    // Text field for the new name of the field to be refactored
+    const newNameTextField = await vscode.window.showInputBox({
+      prompt: newPrompt,
+      placeHolder: newPlaceholder,
+    });
+
+    if (!newNameTextField) {
+      return; // User canceled the input
+    }
+
+    // Process the refactoring using the selected option and inputs
+    vscode.window.showInformationMessage(`Selected Option: ${selectedOption}, Original Name: ${originalNameTextField}, New Name: ${newNameTextField}`);
+
+    // Call the CLI for REM :D
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+// Function to get prompt and placeholder text for the original name
+function getPandP_original(selectedOption: string): { promptText: string, placeholderText: string } {
+  let promptText = '';
+  let placeholderText = '';
+
+  switch (selectedOption) {
+    case 'Variable':
+      promptText = 'Enter the name of the variable to be refactored';
+      placeholderText = 'Variable name';
+      break;
+    case 'Function (fn)':
+      promptText = 'Enter the name of the function to be refactored';
+      placeholderText = 'Function name';
+      break;
+    case 'Struct':
+      promptText = 'Enter the name of the struct to be refactored';
+      placeholderText = 'Struct name';
+      break;
+    case 'Trait':
+      promptText = 'Enter the name of the trait to be refactored';
+      placeholderText = 'Trait name';
+      break;
+    case 'Implementation (impl)':
+      promptText = 'Enter the name of the impl to be refactored';
+      placeholderText = 'Implementation name';
+      break;
+    default:
+      promptText = 'Enter the name of the item to be refactored';
+      placeholderText = 'Item name';
+  }
+
+  return { promptText, placeholderText };
+}
+
+// Function to get prompt and placeholder text for the new name
+function getPandP_new(selectedOption: string): { promptText: string, placeholderText: string } {
+  let promptText = '';
+  let placeholderText = '';
+
+  switch (selectedOption) {
+    case 'Variable':
+      promptText = 'Enter the new name for the variable';
+      placeholderText = 'New variable name';
+      break;
+    case 'Function (fn)':
+      promptText = 'Enter the new name for the function';
+      placeholderText = 'New function name';
+      break;
+    case 'Struct':
+      promptText = 'Enter the new name for the struct';
+      placeholderText = 'New struct name';
+      break;
+    case 'Trait':
+      promptText = 'Enter the new name for the trait';
+      placeholderText = 'New trait name';
+      break;
+    case 'Implementation (impl)':
+      promptText = 'Enter the new name for the implementation';
+      placeholderText = 'New implementation name';
+      break;
+    default:
+      promptText = 'Enter the new name for the item';
+      placeholderText = 'New item name';
+  }
+
+  return { promptText, placeholderText };
+}
